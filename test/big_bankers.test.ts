03b99e5c027d618915643b6df187858de291abf2
gilluan formiga <gilluan.formiga@gmail.com>
2022-10-26 12:45:54 +0200

    chore: initial commit

---
+++ test/big_bankers.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BigBankers from '../lib/big_bankers-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/big_bankers-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BigBankers.BigBankersStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
